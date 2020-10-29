2a837268036f61d00f3693316621dad83265fbee
Mario Arias Gómez <msc.mario.sistemas@gmail.com>
2020-10-29 11:09:00 -0600

    update

--- DM_REP05_WPD-master/src/java/mx/dao/DAO.java
+++ DM_REP05_WPD-master/src/java/mx/dao/DAO.java
@@ -29,8 +29,8 @@ public class DAO {
     public void Conectar() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            //cn = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-557O6CE\\SQLEXPRESS;databaseName=SAE70Empre01", "sa", "dmsistemas");
-            cn = DriverManager.getConnection("jdbc:sqlserver://192.168.1.37\\SQLEXPRESS;databaseName=SAE70Empre01", "sa", "Aspel**2013");
+            cn = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-557O6CE\\SQLEXPRESS;databaseName=SAE70Empre01", "sa", "dmsistemas");
+            //cn = DriverManager.getConnection("jdbc:sqlserver://192.168.1.37\\SQLEXPRESS;databaseName=SAE70Empre01", "sa", "Aspel**2013");
         } catch (ClassNotFoundException | SQLException e) {
         }
 
@@ -52,8 +52,8 @@ public class DAO {
     public void Conectarprov() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            //cnprov = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-557O6CE\\SQLEXPRESS;databaseName=PortalProvNac", "sa", "dmsistemas");
-            cnprov = DriverManager.getConnection("jdbc:sqlserver://WIN-2M3H0PDH8W8;databaseName=PortalProvNac", "sa", "duch3!\"#2020");
+            cnprov = DriverManager.getConnection("jdbc:sqlserver://DESKTOP-557O6CE\\SQLEXPRESS;databaseName=PortalProvNac", "sa", "dmsistemas");
+            //cnprov = DriverManager.getConnection("jdbc:sqlserver://WIN-2M3H0PDH8W8;databaseName=PortalProvNac", "sa", "duch3!\"#2020");
         } catch (ClassNotFoundException | SQLException e) {
         }
 
